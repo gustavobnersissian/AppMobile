488cc063b70dec527959d054ec617f668a19d5c0
GranFireball <leonardohk10@hotmail.com>
2022-06-14 17:29:05 -0300

    Commit leo

--- app/src/main/java/com/example/projetomobile/Comunidade.kt
+++ app/src/main/java/com/example/projetomobile/Comunidade.kt
@@ -1,11 +1,31 @@
 package com.example.projetomobile
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projetomobile.databinding.ActivityComunidadeBinding
 
 class Comunidade : AppCompatActivity() {
+    lateinit var binding: ActivityComunidadeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comunidade)
+        binding = ActivityComunidadeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.add.setOnClickListener {
+            val i = Intent(this, NewPost::class.java)
+            startActivityForResult(i, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1){
+            if(resultCode == Activity.RESULT_OK){
+
+            }
+        }
     }
 }
